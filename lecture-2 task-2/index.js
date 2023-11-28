6986fa0e275ec48757d3b86bd85d515accfd25b4
Shiblu-islam <shibluislam95@gmail.com>
2023-11-29 01:08:42 +0600

    first commit

---
+++ lecture-2 task-2/index.js
@@ -0,0 +1,26 @@
+// area of circle
+let pi = 3.1416
+let r = Number(prompt('Enter Radius value: '))
+
+let areaOfCircle = pi * (r**2)
+document.write('Area of circle: ' + areaOfCircle+"<br/>")
+
+// area of square
+
+let a = Number(prompt('Enter value of A: '))
+
+areaOfSquare = a**2
+document.write('area of square: ' + areaOfSquare+"<br/>")
+
+let height1 = Number(prompt('Enter the length: '))
+let width = Number(prompt('Enter the width: '))
+
+let areaOfRectangle = width * height1
+document.write('Area of rectangle: ' + areaOfRectangle+"<br/>")
+
+
+let base = Number(prompt('Enter the base: '))
+let height = Number(prompt('Enter the height: '))
+
+let areaOfTriangle = 0.5 * base * height
+document.write('Area of triangle: ' + areaOfTriangle+"<br/>")
